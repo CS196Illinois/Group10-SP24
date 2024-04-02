3f934290e21671b23b3028832bce5dbcf84f82a7
Your Name <achou27@illinois.edu>
2024-04-02 17:18:57 -0500

    ShopScreen1.0

--- src/pages/ShopScreen.js
+++ src/pages/ShopScreen.js
@@ -1,14 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+// import { supabase } from './initSupabase';
 import React from 'react'
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 const ShopScreen = () => {
-  return (
-	<View>
-	  <Text>ShopScreen</Text>
-	</View>
-  )
-}
+    return (
+      <View style={styles.container}>
+        <LinearGradient
+            colors={['rgb(103,240,213)', 'rgb(206,251,175)']}
+            style={styles.background}
+        />
+      </View>
+    )
+  }
 
-export default ShopScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'orange',
+  },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 300,
+  },
+  button: {
+    padding: 15,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  text: {
+    backgroundColor: 'transparent',
+    fontSize: 15,
+    color: '#fff',
+  },
+});
 
-const styles = StyleSheet.create({})
+
+export default ShopScreen;
